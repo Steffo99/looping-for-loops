c8342b9e1cd4c2115e6bb873d74800eb2f57684e
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-10-04 17:48:28 +0200

    🐞 Fix ScrollParents not properly inheriting movement speed

--- Objects/ScrollingObjects/Utilities/ConveyorScrollParent.gd
+++ Objects/ScrollingObjects/Utilities/ConveyorScrollParent.gd
@@ -2,12 +2,13 @@ extends ScrollParent
 class_name ConveyorScrollParent
 
 
-onready var conveyor_belt: ConveyorBelt = null
+var conveyor_belt: ConveyorBelt = null
 
 
-func _enter_tree():
+func _ready():
 	conveyor_belt = get_tree().current_scene.get_node("ConveyorBelt")
+	_on_ConveyorBelt_cb_speed_changed(0, conveyor_belt.cb_speed)
 	conveyor_belt.connect("cb_speed_changed", self, "_on_ConveyorBelt_cb_speed_changed")
 
 func _on_ConveyorBelt_cb_speed_changed(old, new):
-	scroll_velocity.x = -conveyor_belt.get_relative_cb_speed(parent.position)
+	scroll_velocity.x = -new
--- Objects/ScrollingObjects/Utilities/ScrollParent.gd
+++ Objects/ScrollingObjects/Utilities/ScrollParent.gd
@@ -2,7 +2,7 @@ extends Node
 class_name ScrollParent
 
 
-export(Vector2) var scroll_velocity: Vector2 = Vector2(-100, 0)
+export(Vector2) var scroll_velocity: Vector2 = Vector2(0, 0)
 var parent = null
 
 
