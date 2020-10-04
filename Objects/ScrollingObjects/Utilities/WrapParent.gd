f8f935a99a7c81f116c6ffe421806efa47333520
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-10-04 17:29:28 +0200

    ❇️ Add object wrapping

---
+++ Objects/ScrollingObjects/Utilities/WrapParent.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name WrapParent
+
+
+onready var parent: Node2D = get_parent()
+onready var wrapper: Wrapper = get_tree().current_scene.get_node("Wrapper")
+onready var absolute_position: Vector2 = parent.position
+
+
+func _physics_process(delta):
+	if global_position.x <= 0:
+		parent.position.x += wrapper.total_length
---
+++ Objects/SpecialObjects/Wrapper.gd
@@ -0,0 +1,5 @@
+extends Node
+class_name Wrapper
+
+
+export(float, 1500, 20000) var total_length: float = 1500
