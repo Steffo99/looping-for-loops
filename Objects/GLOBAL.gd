d175e42eb847b08132f0129d5a864a1434b5920e
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-10-06 12:02:10 +0200

    Fix RNG not working on Firefox

--- Objects/GLOBAL.gd
+++ Objects/GLOBAL.gd
@@ -2,3 +2,8 @@ extends Node
 
 
 var loops_collected: int = 0
+var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
+
+func _init():
+	rng.randomize()
--- Objects/ScrollingObjects/Loop.gd
+++ Objects/ScrollingObjects/Loop.gd
@@ -1,15 +1,12 @@
 extends Area2D
 class_name Loop
 
-var rng = RandomNumberGenerator.new()
-
 
 signal picked_up
 
 
 func _ready():
-	rng.randomize()
-	$Sprite.self_modulate = Color.from_hsv(rng.randf_range(0.0, 1.0), 0.4, 1)
+	$Sprite.self_modulate = Color.from_hsv(GLOBAL.rng.randf_range(0.0, 1.0), 0.4, 1)
 
 
 func _on_Loop_body_entered(body):
